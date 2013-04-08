dbd4485de3bfc15a4620592d6e2343b2e023f860
Guy John <guy@rumblesan.com>
2013-04-08 20:52:51 +0100

    Splitting Manager out into a separate file

--- src/main/scala/com/rumblesan/scalapd/PureData.scala
+++ src/main/scala/com/rumblesan/scalapd/PureData.scala
@@ -1,54 +1,9 @@
 package com.rumblesan.scalapd
 
-import com.rumblesan.scalapd.network.{PDComs, PDConnection, PDMessage, PDChannel}
-
 import akka.actor._
 
 
-/** This is the actor that starts the PD process as well as handling message passing
-  * between it and the rest of the app
-  */
-
-class PureDataManager(port: Int) extends Actor {
-
-  val pdProcess:ActorRef = context.actorOf(Props[PureData])
-  val pdComs = new PDComs(port, self)
-
-  var running: Boolean = false
-
-  var channel: PDChannel = null
-
-  def receive = {
-
-    case StartPD(exe, port, patch, paths, extraArgs) => {
-      if (running) {
-        println("Already Running")
-      } else {
-        pdProcess ! StartPD(exe, port, patch, paths, extraArgs)
-
-        running = true
-      }
-    }
-
-    case PDConnection(connection) => {
-      channel = connection
-    }
-
-    case SendPDMessage(message) => {
-      channel.write(message)
-    }
-
-    case PDMessage(message) => {
-      println("Message from PD:  %s".format(message))
-    }
-
-  }
-
-}
-
 case class SendPDMessage(message: List[String])
-
-
 
 
 trait FileLogger {
